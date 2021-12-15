1a3fa5229a088c32a320b624ba5e41e799cab795
Mike <mclancy96@gmail.com>
2021-12-15 15:16:09 -0500

    initial setup

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
-const methodOverride = require("method-override");
-const random = require('random');
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
 
 app.get("/", function(req,res){
 	res.render("index");
